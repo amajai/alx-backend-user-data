62cee660a922037541ae90b79f127f2aeed1db85
Abdul <aabdulmajeed.isa@gmail.com>
2024-04-19 03:14:13 +0100

    done exercise

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""info """
+from typing import List, TypeVar
+from flask import request
+
+
+class Auth:
+    """info"""
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """info"""
+        if path is None or excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        if path[-1] != "/":
+            path += "/"
+        return path not in excluded_paths
+
+    def authorization_header(self, request=None) -> str:
+        """info"""
+        if request is None:
+            return None
+        return request.headers.get("Authorization", None)
+
+    def current_user(self, request=None) -> TypeVar("User"):
+        """info"""
+        return None
---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""basic auth"""
+
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """basic auth"""
+    pass
